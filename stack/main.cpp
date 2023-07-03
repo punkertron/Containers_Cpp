06ec94f2325fdb678c497eae97150e0813edeb01
punkertron <vascherbatov@mail.ru>
2023-07-03 17:28:59 +0400

    some changes in stack

--- stack/main.cpp
+++ stack/main.cpp
@@ -11,5 +11,9 @@ int main()
     std::cout << i.top() << std::endl;
     i.top() = 100;
     std::cout << i.top() << std::endl;
+    if (!i.empty())
+        std::cout << "H\n" << i.size() << std::endl;
+    i.pop();
+    std::cout << i.top() << '|' << i.size() << std::endl;
     return 0;
 }
--- stack/stack.hpp
+++ stack/stack.hpp
@@ -4,36 +4,67 @@
 #include <deque>
 
 namespace ft {
-
+                                    //REPLACE IN THE FUTURE
 template <class T, class Container = std::deque<T> > class stack
 {
 public:
     typedef typename Container::value_type		value_type;
-    //typedef typename Container::reference		reference;
-    //typedef typename Container::const_reference	const_reference;
-    typedef size_t		size_type;
+    typedef typename Container::reference		reference;
+    typedef typename Container::const_reference	const_reference;
+    typedef typename Container::size_type		size_type;
     typedef	         Container			        container_type;
 
 protected:
     container_type c;
 
 public:
-    void push(const value_type& val)
+    
+    ~stack()
     {
-        c.push_back(val);
+        ;
     }
-
-
+    
+    
+    //Element access
     const value_type& top() const
     {
         return c.back();
     }
+
     value_type& top()
     {
         return c.back();
     }
+    
+    //Capacity
+    bool empty() const
+    {
+        return c.empty();
+    }
+
+    size_type size() const
+    {
+        return c.size();
+    }
+
+    //Modifiers
+    void push(const value_type& val)
+    {
+        c.push_back(val);
+    }
+    //void push( value_type&& value );
+
+    //template< class... Args >
+    //void emplace( Args&&... args );
+
+    void pop()
+    {
+        c.pop_back();
+    }
+    //void swap( stack& other ) noexcept(/* see below */);
+
 };
 
 } //namespace ft
 
-#endif
+#endif //STACK_HPP
